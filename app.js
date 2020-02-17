9053ba0878adf8750fd29087e058ac2b6b8ede9b
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-02-17 14:35:52 +0200

    Added views and styles; bootstrap

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const {
-  PORT
+  PORT,
+  DB_PORT,
+  DB_NAME
 } = process.env;
 
-console.log(`port is ${PORT}`);
+// Connect to db
+mongoose.connect(`mongodb://localhost:${DB_PORT}/${DB_NAME}`,
+  { useNewUrlParser: true, useUnifiedTopology: true }).then(
+    () => {
+      console.log('Connected to db');
+    },
+    err => {
+      console.log('Cant connect to db');
+    }
+  );
 
 // Init app
 const app = express();
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', ejs);
+app.set('view engine', 'ejs');
 
 //Set public folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Home'
+  });
+});
 
 //Start the server
 app.listen(PORT, () => {
